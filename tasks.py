68d530febfdbf5b2e75e7554dfb8e5924184b72e
Jeff Forcier <jeff@bitprophet.org>
2018-06-18 19:22:16 -0700

    Set up for blackening

--- tasks.py
+++ tasks.py
@@ -1,9 +1,9 @@
 from invoke import Collection
-from invocations import docs
+from invocations import docs, travis, checks
 from invocations.packaging import release
 
 
-ns = Collection(release, docs)
+ns = Collection(release, docs, travis, checks.blacken)
 ns.configure({
     'packaging': {
         'sign': True,
