137c785369d2ef026d1bccb14b7d2dc75ec975f0
Jeff Forcier <jeff@bitprophet.org>
2018-06-18 21:53:08 -0700

    Blacken with black 18.5b0

--- tasks.py
+++ tasks.py
@@ -4,10 +4,12 @@ from invocations.packaging import release
 
 
 ns = Collection(release, docs, travis, checks.blacken)
-ns.configure({
-    'packaging': {
-        'sign': True,
-        'wheel': True,
-        'changelog_file': 'docs/changelog.rst',
+ns.configure(
+    {
+        "packaging": {
+            "sign": True,
+            "wheel": True,
+            "changelog_file": "docs/changelog.rst",
+        }
     }
-})
+)
