96bb1c2ae28333046e2a81f9dfcc1e21aa7624d2
Jeff Forcier <jeff@bitprophet.org>
2022-09-08 17:41:11 -0400

    Don't import deprecated module

--- tasks.py
+++ tasks.py
@@ -1,9 +1,9 @@
 from invoke import Collection
-from invocations import docs, travis, checks
+from invocations import docs, checks
 from invocations.packaging import release
 
 
-ns = Collection(release, docs, travis, checks.blacken)
+ns = Collection(release, docs, checks.blacken)
 ns.configure(
     {
         "packaging": {
